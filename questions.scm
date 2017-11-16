cbc00f72768e465995e703c86aca47e29fb1e527
Louie McConnell <louie_mc@Louies-MacBook-Pro.local>
2017-11-15 21:53:52 -0800

    progress on 18

--- questions.scm
+++ questions.scm
@@ -11,7 +11,7 @@
   ) 
   (if (null? rests) 
     nil 
-    (append (list (addFirst first (car rests))) (cons-all first (cdr rests))))
+    (append (list (addFirst first (car rests))) (cons-all first (cdr rests)))
   )
 )
 
@@ -39,28 +39,71 @@
 ;; List all ways to make change for TOTAL with DENOMS
 (define (list-change total denoms)
   ; BEGIN PROBLEM 18
-  (cons-all 1 '((2 4) (1 5) (6 3)))
-;  (cond 
-;    ; if total == 0
-;    ((= total 0) (list (list (car denoms))))
-;    ; (append does nothing 
-;    ((< total 0) nil) 
-;    ; if the total is still greater than 0 and you have no denoms left, no combos
-;    ((null? denoms) nil) 
-;    ; if the current denom is bigger than total, then call helper on rest. 
-;    ((> (car denoms) total) (list-change total (cdr denoms)))
-;    ; if the first denom is equal to the total, 
-;    ; ((= (car denoms) total) )
-;    ; this means you have combos. 
-;    (else 
-;      (append   
-;        (cons-all (car denoms) (list-change (- total (car denoms)) denoms))
-;        (list-change total (cdr denoms))
-;      )
+  (define (helper tot den) 
+    (cond 
+      ; if total == 0
+      ((= tot 0) (list (list (car den))))
+      ; (append does nothing 
+      ((< tot 0) nil) 
+      ; if the total is still greater than 0 and you have no denoms left, no combos
+      ((null? den) nil) 
+      ; if the current denom is bigger than total, then call helper on rest. 
+      ((> (car den) tot) (list-change tot (cdr den)))
+      ; if the first denom is equal to the total, 
+      ; ((= (car denoms) total) )
+      ; this means you have combos. 
+      (else 
+        (append   
+          (cons-all (car den) (helper (- tot (car den)) den))
+          (helper tot (cdr den))
+        )
+      )
+    )
+  )
+  (define output (helper total denoms))
+  (define (removeLast lst) 
+    ;(display lst)
+    ;(newline)
+    (if (eq? '() (cdr lst)) 
+      '() 
+      (cons (car lst) (removeLast (cdr lst))) 
+    )
+  ) 
+  (display output)
+
+  (define first (list 10 10 10 10 10 10))
+  ; (display first)
+  (newline)
+  (define second (cadr output))
+  ; (display second) 
+  (newline)
+  
+  ;(display (removeLast first))
+;  (display (cdr first))
+;  (newline)
+;  (display (car first))
+;  (newline)
+;  (display (car (cdr first)))
+;  (newline)
+;  (display (cdr (cdr first)))
+;  (display (eq? '() (cddr first)))
+;  (newline)
+;  (display first)
+;  (removeLast '(5 5 5))
+;  (removeLast '(10 10))
+;  (define (listMap proc lst) 
+;    (if (null? lst) 
+;      nil
+;      (append (list (proc (car lst))) (listMap proc  (cdr lst))) 
 ;    )
-   
+;  ) 
+  ; (listMap removeLast output)
+  ; (map removeLast output)
+  ; (listMap (lambda (lt) (append lt (list 1))) '((1 2) (1 3) (4 5)))
 )
-;  ; END PROBLEM 18
+  
+
+; END PROBLEM 18
 ;
 ;; Problem 19
 ;; Returns a function that checks if an expression is the special form FORM
